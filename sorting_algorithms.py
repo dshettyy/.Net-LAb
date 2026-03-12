7529d061edb23419edcfa0fd55da4d7c55118531
dshettyy <dishanthshettyy@gmail.com>
2026-03-12 10:28:21 +0530

    Update sorting_algorithms.py

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -153,3 +153,21 @@ print("\nPostorder Traversal:")
 postorder(root)
 print("\nLevel Order Traversal:")
 level_order(root)
+
+
+def quick_sort(arr):
+
+
+if len(arr) <= 1:
+
+return arr
+
+pivot = arr[len(arr) // 2]
+
+left = [x for x in arr if x < pivot]
+
+middle = [x for x in arr if x == pivot]
+
+right = [x for x in arr if x > pivot]
+
+return (quick_sort(left) + middle + quick_sort(right))
